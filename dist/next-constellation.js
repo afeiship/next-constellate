38f0f2b2bf43c0aabdcbf55df7a16340a365b40d
afeiship <1290657123@qq.com>
2017-12-11 23:25:50 +0800

    issue: optimze var

--- dist/next-constellation.js
+++ dist/next-constellation.js
@@ -5,8 +5,8 @@
   var CONSTELLATION_STR = '魔羯水瓶双鱼牡羊金牛双子巨蟹狮子处女天秤天蝎射手魔羯';
   var DATE_ARR = [ 20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22 ];
 
-  nx.constellation = function (month,day) {
-    return CONSTELLATION_STR.substr(month * 2 - (day < DATE_ARR[month - 1]
+  nx.constellation = function (inMonth,inDay) {
+    return CONSTELLATION_STR.substr(inMonth * 2 - (inDay < DATE_ARR[inMonth - 1]
       ? 2
       : 0), 2);
   };
